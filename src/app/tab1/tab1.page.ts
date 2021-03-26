c43ef39c2de7d106fc7c42469219cc7d1cd58c32
Lawrenz-Sese <78797968+Lawrenz-Sese@users.noreply.github.com>
2021-03-26 14:42:16 +0800

    TabsIcon -Tab1Content

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  cardContent: any = [
+    {product: 'Pork', price: '400', img: '../../assets/cart.png'},
+    {product: 'Chicken', price: '180', img: '../../assets/cart.png'},
+    {product: 'Beef', price: '400', img: '../../assets/cart.png'},
+    {product: 'Potato', price: '100', img: '../../assets/cart.png'}
+  ]
   constructor() {}
 
 }
