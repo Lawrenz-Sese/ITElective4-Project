dc1810964d968b3a2b565a1c9b817c7edfa02b5a
Renon <201811347@gordoncollege.edu.ph>
2021-05-30 23:18:37 +0800

    UI updates

--- src/app/history/history.page.ts
+++ src/app/history/history.page.ts
@@ -22,7 +22,7 @@ export class HistoryPage implements OnInit {
 
   constructor( private router: Router, private ds: DataService, private modalCtrl: ModalController, private ev: EventTriggerService ) {
     this.clickEvent = this.ev.getClickEvent().subscribe(()=>{
-      this.pullHist();
+     
     })
    }
 
@@ -53,7 +53,7 @@ export class HistoryPage implements OnInit {
     this.histinfo.user_id = localStorage.getItem("id");
     this.ds.sendApiRequest("hist",localStorage.getItem("id")).subscribe(data => {
     this.hist = data.payload;
-    this.ev.sendClickEvent();
+    
 
     for (let i = 0; i <= this.hist.length; i++) {
       this.histCounter = i;
